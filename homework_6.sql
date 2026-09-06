dcc6e5508d3768f4be19268828df4fe420fe9740
winayaak <vinayak190804@gmail.com>
2026-09-06 07:16:33 +0530

    adding triggers

--- homework_6.sql
+++ homework_6.sql
@@ -16,3 +16,88 @@ select * from products
 where price = (
     select max(price) from products
 );
+
+create table BackupProducts(
+    product_id int,
+    product_name varchar(100),
+    price decimal(10,2),
+    product_category_id int
+);
+
+create or replace trigger backup_products
+after insert on products
+for each row
+begin
+    insert into BackupProducts values(
+        :new.product_id,
+        :new.product_name,
+        :new.price,
+        :new.product_category_id
+    );
+end;
+/
+
+create table ProductHistory(
+    product_id int,
+    product_name varchar(100),
+    price decimal(10,2),
+    product_category_id int
+);
+
+create or replace trigger product_history
+before update on products
+for each row
+begin
+    insert into ProductHistory values(
+        :old.product_id,
+        :old.product_name,
+        :old.price,
+        :old.product_category_id
+    );
+end;
+/
+
+create table DeletedProducts(
+    product_id int,
+    product_name varchar(100),
+    price decimal(10,2),
+    product_category_id int
+);
+
+create or replace trigger deleted_products
+before delete on products
+for each row
+begin
+    insert into DeletedProducts values(
+        :old.product_id,
+        :old.product_name,
+        :old.price,
+        :old.product_category_id
+    );
+end;
+/
+
+create table PriceUpdates(
+    product_id int,
+    old_price decimal(10,2),
+    new_price decimal(10,2),
+    update_timestamp timestamp
+);
+
+create or replace trigger MonitorHighPrices
+before update on products
+for each row
+begin
+    if :new.price > 1000 then
+        insert into PriceUpdates values(
+            :old.product_id,
+            :old.price,
+            :new.price,
+            systimestamp
+        );
+    end if;
+end;
+/
+update products
+set price=20000
+where product_id=1;
